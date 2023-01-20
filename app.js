0ce1b50a061947f90a2c6fcaee473b2b71182304
devrajn <devraj.nepal@gmail.com>
2023-01-20 08:58:35 -0600

    function starting commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const  myFunction =() => {
     document.getElementById("sheldon").innerHTML = "(Sheldon Lee Cooper) James Joseph Parsons (born March 24, 1973)";
   }
+
+  const  amyBio =() => {
+    document.getElementById("amy").innerHTML = "Mayim Chaya Bialik born December 12, 1975) is an American actress, game show host, and author.";
+  }
